85575465c2d328feaafccfcddfd7442180d8617b
Mark Hall <mark.hall993@gmail.com>
2023-10-03 11:15:54 -0400

    Add comments to config

--- config/config.go
+++ config/config.go
@@ -2,22 +2,29 @@ package config
 
 import "os"
 
+// Config represents the configuration structure used by the application.
+// It contains fields for the server host, server port, and JWT secret key.
 type Config struct {
 	ServerHost string
 	ServerPort string
 	JWTSecret  string
 }
 
+// C is the global configuration instance populated by the Load function.
 var C Config
 
+// Load initializes the global configuration (C) using environment variables or default values.
 func Load() {
 	C = Config{
-		ServerHost: getEnv("HOST", "localhost"),
-		ServerPort: getEnv("PORT", "8080"),
-		JWTSecret:  getEnv("JWT_KEY", ""),
+		ServerHost: getEnv("HOST", "localhost"), // Default to localhost if HOST environment variable is not set
+		ServerPort: getEnv("PORT", "8080"),      // Default to port 8080 if PORT environment variable is not set
+		JWTSecret:  getEnv("JWT_KEY", ""),       // No default for JWT secret; it should be set securely in the environment
 	}
 }
 
+// getEnv fetches the value of an environment variable or returns a default value.
+// key is the name of the environment variable to fetch.
+// defaultValue is the value to return if the environment variable is not set.
 func getEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
 		return value
