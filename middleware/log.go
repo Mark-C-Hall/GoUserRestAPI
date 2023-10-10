f08f9eeca91d29fd5cade7808c7e264ed5d02f86
Mark Hall <mark.hall993@gmail.com>
2023-10-10 15:08:31 -0400

    Fix logging bug with timing

--- middleware/log.go
+++ middleware/log.go
@@ -9,7 +9,7 @@ import (
 func LoggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
 		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
 	}
 }
